55b635923e9f0c3f8e4bb3d69349a4055037d39c
Andreas Rigas <andreas.rigas@outlook.com>
2020-03-05 00:35:23 +0000

    refactor project

--- foundations-block/src/main/scala/foundations/Exercises/B3/RecursionFun/RecursionFun.scala
+++ foundations-block/src/main/scala/foundations/Exercises/B3/RecursionFun/RecursionFun.scala
@@ -117,17 +117,9 @@ object RecursionFun extends App {
   //List(a,b,c)
   //List(List(a,b,c),List(a,b),List(a,c),List(a),List(b,c),List(b),List(c),List())
 
-  def combinationsCal(list: List[Char]): Set[List[Char]] = {
-    list match {
-      case Nil         => Set(List())
-      case head :: Nil => Set(List(head))
-      case head :: second :: tail =>
-        (Set(list) ++ combinationsCal(head :: tail)) ++ combinationsCal(
-          second :: tail
-        ) ++ combinationsCal(head :: list.tail.dropRight(1))
-    }
-  }
+  def combinationsCal(list: List[Char]): List[List[Char]] = ???
 
   val comb = combinationsCal(List('a', 'b', 'c'))
   println(comb + "  " + comb.size)
+
 }
--- foundations-block/src/main/scala/foundations/Solutions/B3/RecursionFun/RecursionFun.scala
+++ foundations-block/src/main/scala/foundations/Solutions/B3/RecursionFun/RecursionFun.scala
@@ -1,3 +1,29 @@
 package foundations.Solutions.B3.RecursionFun
 
-object RecursionFun extends App {}
+object RecursionFun extends App {
+
+  //MEGA BONUS
+  //Implement a function that calculates all the possible combinations of a list with chars
+  //For example
+  //List(a,b,c)
+  //List(List(a,b,c),List(a,b),List(a,c),List(a),List(b,c),List(b),List(c),List())
+
+  def combinationsCal(list: List[Char]): List[List[Char]] = {
+
+    def recursionCombin(list: List[Char]): Set[List[Char]] = {
+      list match {
+        case Nil         => Set(List())
+        case head :: Nil => Set(List(head)) ++ recursionCombin(Nil)
+        case head :: second :: tail =>
+          (Set(list) ++ recursionCombin(head :: tail)) ++
+            recursionCombin(second :: tail) ++
+            recursionCombin(head :: list.tail.dropRight(1))
+      }
+    }
+
+    recursionCombin(list).toList
+  }
+
+  val comb = combinationsCal(List('a', 'b', 'c'))
+  println(comb + "  " + comb.size)
+}
